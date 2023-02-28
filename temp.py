8097289489c64ef870cff37c817c121f21b6b65a
srihari50 <116938964+srihari50@users.noreply.github.com>
2023-02-28 14:07:14 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -7,7 +7,19 @@ import pickle
 
 st.title('cement sales Forecasting')  
 
+st.sidebar.title("About")
+st.sidebar.info("Forecastingcement sales 'Prophet' Machine Learning model.")
 
+def get_input():
+	st.sidebar.header("Input From user")
+	st.sidebar.subheader("Select range of Date for visualize data for particular date range.")
+	st.sidebar.write("(From 2023-01-31 to 2023-12-31)")
+	start_date = st.sidebar.text_input("Start Date", "2023-01-31")
+	end_date = st.sidebar.text_input("End Date", "2023-12-31")
+	st.write("")
+	st.sidebar.subheader("Enter Period for Forecasting of Price")
+	period = st.sidebar.text_input("Period (In months)", "12")
+	return start_date, end_date, period
     
     
     
