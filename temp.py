678e5e354c07968b324c4e4d62c15ce3c2a147cd
srihari50 <116938964+srihari50@users.noreply.github.com>
2023-02-28 14:11:46 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -20,6 +20,37 @@ def get_input():
 	st.sidebar.subheader("Enter Period for Forecasting of Price")
 	period = st.sidebar.text_input("Period (In months)", "12")
 	return start_date, end_date, period
+
+START = "2023-01-31"
+TODAY = date.today().strftime("%Y-%M-%d")
+
+def get_data(start, end):
+	df = pd.read_excel('"C:\Users\sriha\Desktop\cement project\All India_Features.xlsx"')
+	start = pd.to_datetime(start)
+	end = pd.to_datetime(end)
+	start_row = 0
+	end_row = 0
+
+	for i in range(0, len(df)):
+		if start <=	pd.to_datetime(df['Date'][i]):
+			start_row = i
+			break
+	for j in range(0, len(df)):
+		if end >= pd.to_datetime(df['Date'][len(df)-1-j]):
+			end_row = len(df) - 1 - j
+			break
+
+	df = df.set_index(pd.DatetimeIndex(df['Date'].values))
+	return df.iloc[start_row:end_row+1, :]
+
+start, end, period = get_input()
+data = get_data(start, end)
+
+st.subheader("Data")
+st.write("First 5 Columns")
+st.write(data.head())
+st.write("Last 5 Columns")
+st.write(data.tail())
     
     
     
