b0da2e9852f58c581455cbfe35e5c8ebccdec319
srihari50 <116938964+srihari50@users.noreply.github.com>
2023-02-28 14:47:21 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,81 +4,35 @@ from datetime import date
 import pandas as pd
 from PIL import Image
 import pickle
+import numpy as np
+from prophet import Prophet
+from prophet.plot import plot_plotly
 
-st.title('cement sales Forecasting')  
+st.title('Cement Sales Forecasting')
 
-st.sidebar.title("About")
-st.sidebar.info("Forecastingcement sales 'Prophet' Machine Learning model.")
+data = st.file_uploader(' ',type='Xlsx')
+if data is not None:
+  appdata = pd.read_excel(data)
+  appdata = appdata.rename(columns={'Sales_Quantity_Milliontonnes': 'y', 'Date':'ds'})
+  appdata['ds'] = pd.to_datetime(appdata['ds']) 
 
-def get_input():
-	st.sidebar.header("Input From user")
-	st.sidebar.subheader("Select range of Date for visualize data for particular date range.")
-	st.sidebar.write("(From 2023-01-31 to 2023-12-31)")
-	start_date = st.sidebar.text_input("Start Date", "2023-01-31")
-	end_date = st.sidebar.text_input("End Date", "2023-12-31")
-	st.write("")
-	st.sidebar.subheader("Enter Period for Forecasting of Price")
-	period = st.sidebar.text_input("Period (In months)", "12")
-	return start_date, end_date, period
+  st.write(appdata)
 
-START = "2023-01-31"
-TODAY = date.today().strftime("%Y-%M-%d")
+  train = df.iloc[:84]
+  test = df.iloc[84:]
+	
+if data is not None:
+     obj = Prophet()
+     obj.fit(test_df)
+  test = test.rename(columns={'Sales_Quantity_Milliontonnes': 'y', 'Date':'ds'})
+  test_df = test.drop(['y'], axis=1)
+  test_forecasts = model.predict(test_df)
 
-uploaded_file = st.file_uploader(" ", type = ['xlsx'])
+test_forecasts = pd.DataFrame(test_forecasts[['yhat', 'yhat_upper', 'yhat_lower', 'Order_Quantity_Milliontonnes']])
+st.write(test_forecast)
 
-if uploaded_file is not None:
-	df = pd.read_excel(Uploaded_file)
-	start = pd.to_datetime(start)
-	end = pd.to_datetime(end)
-	start_row = 0
-	end_row = 0
+figure1 = model.plot(test_forecasts)
+st.write(figure1)
 
-	for i in range(0, len(df)):
-		if start <=	pd.to_datetime(df['Date'][i]):
-			start_row = i
-			break
-	for j in range(0, len(df)):
-		if end >= pd.to_datetime(df['Date'][len(df)-1-j]):
-			end_row = len(df) - 1 - j
-			break
-
-	df = df.set_index(pd.DatetimeIndex(df['Date'].values))
-	return df.iloc[start_row:end_row+1, :]
-
-	start, end, period = get_input()
-	data = get_data(start, end)
-
-	st.subheader("Data")
-	st.write("First 5 Columns")
-	st.write(data.head())
-	st.write("Last 5 Columns")
-	st.write(data.tail())
-
-	st.subheader('Close Price')
-	st.write("Zoom In/Zoom Out for better visualization.")
-	st.line_chart(data[['Open', 'Close']])
-
-	st.subheader("Volume")
-	st.write("Zoom In/Zoom Out for better visualization.")
-	st.line_chart(data['Volume'])
-
-	st.header("Prediction")
-
-	def model_np():
-		m = pickle.load(open('Prophet.pkl', 'rb'))
-
-		st.subheader("Prophet")
-		df = data.copy()
-		df.reset_index(inplace=True)
-		df_train = df[['Date','Close']]
-		df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
-
-		forecast = m.predict(future)
-		forecast = forecast.rename(columns={"ds": "Date", "yhat1": "Close"})
-		st.write("Forecasting of Etheruem Close Price from 14-09-2021 to 18-09-2021")
-		st.write(forecast[['Date', 'Close']].head())
-		st.write(f"Forecasting of Close Price of {period} days")
-		st.line_chart(forecast['Close'])
-
-	model_np()
-
+figure2 = plot_plotly(model, test_forecasts)
+st.write(figure2)
