feaa83a36cbc58ade14b3eecadfde2858b477841
srihari50 <116938964+srihari50@users.noreply.github.com>
2023-03-10 15:44:23 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -21,7 +21,7 @@ if data is not None:
   
   from pandas_profiling import ProfileReport
   
-  profile = ProfileReport(df, tsmode=True, sortby="Date")
+  profile = ProfileReport(df, tsmode=True, sortby="ds")
   st.title("Pandas Profiling Report")
   st_profile_report(profile)
   
